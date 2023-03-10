6edcfd8a151b6671f00388192710d03f18b7b144
Yonus Billow <98870419+ybillow@users.noreply.github.com>
2023-03-10 05:59:04 +0000

    added details

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
 <head>
-<title>Bond Web Service Demo</title>
+<title>Painting Web Service</title>
 <style>
 	body {font-family:georgia;}
-  .film{
+    .film{
     border:1px solid #E77DC2;
     border-radius: 5px;
     padding: 5px;
     margin-bottom:5px;
-    position:relative;   
+    position:relative;
   }
 
 
@@ -17,38 +17,32 @@
     right:10px;
     top:10px;
   }
+
   .pic img{
 	max-width:50px;
   }
 
-
-
+  
 </style>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 <script type="text/javascript">
 
-  function bondTemplate(film){
-    return `
+function bondTemplate(film){
+  return `
     <div class="film">
-          <b>film</b>: ${film.Film}<br>
-          <b>Title</b>: ${film.Title}<br>
-          <b>Director</b>: ${film.Director}<br>
-          <b>Producers</b>: ${film.Producers}<br>
-          <b>Writers</b>: ${film.Writers}<br>
-          <b>Composer</b>: ${film.Composer}<br>
-          <b>Bond</b>: ${film.Bond}<br>
-          <b>Budget</b>: ${film.Budget}<br>
-          <b>Box Office</b>: ${film.BoxOffice}<br>
-     <div class="pic"><img src="thumbnails/${film.Image}"></div>
-      </div>
-
-    `;
-  }
-
+			<b>Athlete</b>: ${film.Athlete}<br>
+      <b>YearsPro</b>: ${film.YearsPro}<br>
+      <b>League</b>: ${film.League}<br>
+      <b>Team</b>: ${film.Team}<br>
+      <b>Championships</b>: ${film.Championships}<br>
+      <b>Hometown</b>: ${film.Hometown}<br>
+      <div class="pic"><img src="thumbnails/${film.Image}" ></div>
+		</div>
+  `;
+}
   
-$(document).ready(function() { 
-
+$(document).ready(function() {  
 
  $('.category').click(function(e){
    e.preventDefault(); //stop default action of the link
@@ -89,26 +83,25 @@ alert('Error - ' + xhr.status + ': ' + xhr.statusText);
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service</h1>
-		<a href="year" class="category">Bond Films By Year</a><br />
-		<a href="box" class="category">Bond Films By International Box Office Totals</a>
-		<h3 id="filmtitle">Title Will Go Here</h3>
+	<h1>Basketball Web Service</h1>
+		<a href="year" class="category">Athletes by Titles Descending</a><br />
+		<a href="box" class="category">Athletes by YearsPro Descending</a>
+		<h3 id="filmtitle"></h3>
 		<div id="films">
       <!--
-			<div class="film">
-        <b>film</b>: 1<br>
-          <b>Title</b>: Dr. No<br>
-          <b>Director</b>: Terence Young<br>
-          <b>Producers</b>: Harry Saltzman and Albert R.     Broccoli<br>
-          <b>Writers</b>: Richard Maibaum, Johanna Harwood and Berkely Mather<br>
-          <b>Composer</b>: Monty Norman<br>
-          <b>Bond</b>: Sean Connery<br>
-          <b>Budget</b>: $1,000,000.00<br>
-          <b>BoxOffice</b>: $59,567,035.00<br>
-      <div class="pic"><img src="thumbnails/dr-no.jpg"></div>
-      </div>
-      -->
+			<b>Film</b>: 1<br>
+      <b>Title</b>: Dr. No<br>
+      <b>Director</b>: Terence Young<br>
+      <b>Producers</b>: Harry Saltzman and Albert R. Broccoli<br>
+      <b>Writers</b>: Richard Maibaum, Johanna Harwood and Berkely Mathe<br>
+      <b>Composer</b>: Monty Norman<br>
+      <b>Bond</b>: Sean Connery<br>
+      <b>Budget</b>: $1,000,000.00<br>
+      <b>BoxOffice</b>: $59,567,035.00<br>
+      <b>Film</b>: 1<br>
+      <div class="pic"><img src="thumbnails/dr-no.jpg" ></div>
 		</div>
-		<div id="output">Results go here</div>
+        -->
+		<div id="output"></div>
 	</body>
 </html>
